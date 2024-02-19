7d993b616f0ef8faaf10d7ea20c3ff14fbb33627
unknown <jordan.conot@gmail.com>
2024-02-20 00:49:13 +0100

    feat: style: page choice

--- src/pages/Choice.jsx
+++ src/pages/Choice.jsx
@@ -1,14 +1,15 @@
-
 import { NavLink } from 'react-router-dom';
 
 const Choice = () => {
     return (
+        <div className='choice_container'>
         <div className='choice'>
-            <h2>Veuillez choisir un utilisateur:</h2>
-            <div className=''>
+            <h2>Veuillez choisir un utilisateur :</h2>
+            <div className='choice_name'>
                 <NavLink to='/user/12'>Karl</NavLink>
                 <NavLink to='/user/18'>Cecilia</NavLink>
             </div>
+        </div>
         </div>
     );
 };
